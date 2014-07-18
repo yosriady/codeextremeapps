945f47744f37ab7dfa79510f3cda9bf120809ab8
Yos Riady <yosriady@gmail.com>
2014-07-19 06:49:45 +0800

    More history work

--- rum/app/js/history.js
+++ rum/app/js/history.js
@@ -1,16 +1,30 @@
 var RUM = RUM || {};
 RUM.History = (function(window, document, $, _) {
 
+    var DATA_URL = "https://rum.firebaseio.com/events";
+    var TIME_INTERVAL = 1000;
+    var dataRef = new window.Firebase(DATA_URL);
+
     function init(){
         // TODO
     }
 
+    function search(){
+        // TODO
+        // Gaoxiang, For filtering, see https://www.firebase.com/docs/queries.html
+        // And, https://www.firebase.com/blog/2013-10-01-queries-part-one.html
+        // Finally, https://www.firebase.com/blog/2014-01-02-queries-part-two.html
+    }
+
     function getData(){
-        alert("Not Implemented!");
+        dataRef.on("value", function(snapshot) {
+            console.log(snapshot.val());
+        });
     }
 
     var HISTORY = {
         init: init,
+        search: search,
         getData: getData
     }
     return HISTORY;
