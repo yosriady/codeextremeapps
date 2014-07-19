2dec4e6b436fad3a11151f2e6f7d1bd836e01fa4
Yos Riady <yosriady@gmail.com>
2014-07-19 12:00:30 +0800

    Scatter chart

--- rum/app/js/history.js
+++ rum/app/js/history.js
@@ -23,6 +23,24 @@ RUM.History = (function(window, document, $, _) {
             var scatterContainer = document.getElementById('scatter');
             var timeLineChart = new google.visualization.Timeline(timeLineContainer);
             var scatterChart = new google.visualization.ScatterChart(scatterContainer);
+
+            var data = new google.visualization.DataTable();
+            data.addColumn('number', 'Time');
+            data.addColumn('number', 'Motion');
+            data.addColumn('number', 'Camera');
+
+            for (var i=0; i<json_data.length; i++) {
+                if (json_data.type == "motion"){
+                    data.addRow([json_data.time, 1, null])
+                } else if (json_data.type == "camera"){
+                    data.addRow([json_data.time, null, 1])
+                }
+            }
+            scatterChart.draw(data, {title: 'Number of Events over Time',
+                      width: 600, height: 400,
+                      vAxis: {title: "Number of Events", titleTextStyle: {color: "green"}},
+                      hAxis: {title: "Time", titleTextStyle: {color: "green"}}}
+            );
         });
     };
 
@@ -33,7 +51,8 @@ RUM.History = (function(window, document, $, _) {
     }
 
     var HISTORY = {
-        init: init
+        init: init,
+        drawChart: drawChart
     };
     return HISTORY;
 
