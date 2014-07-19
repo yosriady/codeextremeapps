9b51650143d260e650f06fc1a041d2cf02cade34
Yos Riady <yosriady@gmail.com>
2014-07-19 12:41:24 +0800

    Add scatter plot visualization

--- rum/app/js/history.js
+++ rum/app/js/history.js
@@ -5,14 +5,7 @@ RUM.History = (function(window, document, $, _) {
     var DATA_URL = "https://rum.firebaseio.com/events";
     var END_TIME = moment().unix();
     var START_TIME = moment().subtract('hours', 2).unix();
-
     var dataRef = new window.Firebase(DATA_URL);
-
-
-    function init() {
-        window.google.load("visualization", "1", {packages:["corechart", "timeline"]});
-        window.google.setOnLoadCallback(drawChart);
-    };
 
     function drawChart() {
         dataRef.startAt(START_TIME)
